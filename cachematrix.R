5bf86c123bdab91e25a4e847b4bd8290f6f45b05
abhi7632 <myanalyzed@gmail.com>
2014-09-22 03:36:50 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code is for caching the inverse of a matrix. There are 2 functions used :-
 
-## Write a short comment describing this function
+## (1) makeCacheMatrix function is used for getting and setting the matrix and its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+## (2) cacheSolve function is used to retrieve the value of the inverse from the  cache
+##     if it exsists there otherwise set the value of the matrix and its inverse in the
+##     cache using makeCacheMatrix function.
 
+
+
+## makeCacheMatrix function has  functions for getting and setting the value of the matrix
+## and its inverse. Then alist of these function objects is created which will be used to 
+## access them from cacheSolve function.
+
+
+makeCacheMatrix <- function(maty=matrix()) {
+    inv <- NULL
+    set <- function(maty) {
+        matx <<- maty
+        inv <<- NULL
+    }
+    get <- function() matx
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function returns inverse from the cache if the inverse exsists there 
+## otherwise it will use the list of functions created in makeCacheMatrix function
+## to set the value of matrix and its inverse in the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(maty=matrix(),func) {
+    inv <- func$getinv()
+    if(!is.null(inv)) {
+        message("getting cached Inverse")
+        return(inv)
+    }
+    
+    func$set(maty)
+    data <- func$get()
+    inv <- solve(data)
+    func$setinv(inv)
+    inv
 }
+
+
+func <- makeCacheMatrix(maty)
+maty <- matrix(data = c(1,2,3,0,1,4,5,6,0),nrow = 3,ncol = 3,byrow = TRUE)
+slv <- cacheSolve(maty,func)
+slv
+
